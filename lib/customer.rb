4fee81c5f2a435b14d4a5ddae7426fb696658e48
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-04-02 22:48:39 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
+    attr_accessor :name, :age, :waiter
   
-end
+    @@all = []
+  
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def new_meal(waiter, total, tip = 0)
+      Meal.new(waiter, self, total, tip) #passing #self (Customer instance) as and argument for each new_meal
+    end
+  
+    def meals
+      Meal.all.select do |meal| #customer looks through all meals and...
+          meal.customer == self #selects the ones that belong to the them
+      end
+    end
+  
+    def waiters
+      meals.map do |meal|
+          meal.waiter
+      end 
+    end
+  
+  end 
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,37 @@
 class Waiter
 
-end
+    attr_accessor :name, :yrs_experience #why don't we need an attr_ for Meal???
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip = 0)
+        Meal.new(self, customer, total, tip) #QUESTION: why does the instance of self come first compared to the customer new_meal instance method?
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+
+    #ask about this entire instance method
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b| #iteration to find the largest of two arrays
+            meal_a.tip <=> meal_b.tip #spaceship operator compares two arrays
+        end
+       
+        best_tipped_meal.customer #return the result of customer for the iteration
+    end
+
+end 
